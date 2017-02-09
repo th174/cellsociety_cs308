ef28f81520c30ecc2c1cac95f440bce41c9b8e8a
Salo Abraham Bandel <salo.abraham@duke.edu>
2017-02-09 17:01:29 -0500

    refactor sea26 dkp 13

--- src/CellSociety/PredatorPrey/PredatorPrey_Cell.java
+++ src/CellSociety/PredatorPrey/PredatorPrey_Cell.java
@@ -11,82 +11,85 @@ import java.util.stream.Collectors;
  */
 public class PredatorPrey_Cell extends Abstract_Cell<PredatorPrey_CellState> {
 
-    public PredatorPrey_Cell(int x, int y, String... params) {
-        super(x, y, new PredatorPrey_CellState(params));
-    }
+	public PredatorPrey_Cell(int x, int y, String... params) {
+		super(x, y, new PredatorPrey_CellState(params));
+	}
 
-    /**
-     * Every turn of the simulation a fish will move to a random adjacent cell
-     * unless all four are occupied. If the fish has survived the number of turns
-     * necessary to breed it produces a new fish if there is an empty adjacent cell..
-     * <p>
-     * Each turn if there is a fish adjacent to a shark the shark eats it. If there are
-     * multiple adjacent fish the shark eats one at random. If there are no adjacent fish
-     * the shark moves in a random direction. After eating or moving if the shark has
-     * survived the number of turns necessary to breed it produces a new shark if there is
-     * an empty adjacent cell.
-     *
-     * @see Abstract_Cell#interact()
-     */
-    @Override
-    public void interact() {
-        if (getTimelineIndex() % 2 == 1 && getCurrentState().equals(PredatorPrey_CellState.PREY)) {
-            if (getCurrentState().canReproduce()) {
-                reproduce(getEmptyNeighbor());
-            } else {
-                move(getEmptyNeighbor());
-            }
-        } else if (getTimelineIndex() % 2 == 0 && getCurrentState().equals(PredatorPrey_CellState.PREDATOR)) {
-            if (getCurrentState().willStarve()) {
-                setNextState(PredatorPrey_CellState.EMPTY);
-            } else {
-                if (!predatorEat(getPreyNeighbor())) {
-                    move(getEmptyNeighbor());
-                }
-                if (getCurrentState().canReproduce()) {
-                    reproduce(getEmptyNeighbor());
-                }
-            }
-        }
-    }
+	/**
+	 * Every turn of the simulation a fish will move to a random adjacent cell
+	 * unless all four are occupied. If the fish has survived the number of turns
+	 * necessary to breed it produces a new fish if there is an empty adjacent cell..
+	 * <p>
+	 * Each turn if there is a fish adjacent to a shark the shark eats it. If there are
+	 * multiple adjacent fish the shark eats one at random. If there are no adjacent fish
+	 * the shark moves in a random direction. After eating or moving if the shark has
+	 * survived the number of turns necessary to breed it produces a new shark if there is
+	 * an empty adjacent cell.
+	 *
+	 * @see Abstract_Cell#interact()
+	 */
+	@Override
+	public void interact() {
+		if (getTimelineIndex() % 2 == 1 && getCurrentState().equals(PredatorPrey_CellState.PREY)) {
+			checkreproduce();
+				move(getEmptyNeighbor());
+			
+		} else if (getTimelineIndex() % 2 == 0 && getCurrentState().equals(PredatorPrey_CellState.PREDATOR)) {
+			if (getCurrentState().willStarve()) {
+				setNextState(PredatorPrey_CellState.EMPTY);
+			} else {
+				if (!predatorEat(getPreyNeighbor())) {
+					move(getEmptyNeighbor());
+				}
+				checkreproduce();
+			}
+		}
+	}
+	//@SuppressWarnings("unused")
+	private void checkreproduce(){
+		if(getCurrentState().canReproduce()){
+			reproduce(getEmptyNeighbor());
+		}
+	}
 
-    private PredatorPrey_Cell getEmptyNeighbor() {
-        Collection<PredatorPrey_Cell> neighbors = getNeighbors().asCollection(PredatorPrey_Cell.class).
-        		stream().filter(PredatorPrey_Cell::nextStateEmpty).collect(Collectors.toSet());
-        return neighbors.stream().skip((long) (Math.random() * neighbors.size())).findAny().orElse(null);
-    }
 
-    private PredatorPrey_Cell getPreyNeighbor() {
-        Collection<PredatorPrey_Cell> potentialFood = getNeighbors().asCollection(PredatorPrey_Cell.class).
-        		stream().filter(e -> e.getCurrentState().equals(PredatorPrey_CellState.PREY)).collect(Collectors.toSet());
-        return potentialFood.stream().skip((long) (Math.random() * potentialFood.size())).findAny().orElse(null);
-    }
+private PredatorPrey_Cell getEmptyNeighbor() {
+	Collection<PredatorPrey_Cell> neighbors = getNeighbors().asCollection(PredatorPrey_Cell.class).
+			stream().filter(PredatorPrey_Cell::nextStateEmpty).collect(Collectors.toSet());
+	return neighbors.stream().skip((long) (Math.random() * neighbors.size())).findAny().orElse(null);
+}
 
-    private boolean predatorEat(PredatorPrey_Cell cell) {
-        if (Objects.nonNull(cell)) {
-            cell.setNextState(PredatorPrey_CellState.EMPTY);
-            setNextState(new PredatorPrey_CellState(getCurrentState(), -1, getCurrentState().
-            		getMaxStarvationTimer()));
-            return true;
-        }
-        return false;
-    }
+private PredatorPrey_Cell getPreyNeighbor() {
+	Collection<PredatorPrey_Cell> potentialFood = getNeighbors().asCollection(PredatorPrey_Cell.class).
+			stream().filter(e -> e.getCurrentState().equals(PredatorPrey_CellState.PREY)).collect(Collectors.toSet());
+	return potentialFood.stream().skip((long) (Math.random() * potentialFood.size())).findAny().orElse(null);
+}
 
-    private void move(PredatorPrey_Cell cell) {
-        if (Objects.nonNull(cell)) {
-            cell.setNextState(getCurrentState().getSuccessorState());
-            setNextState(PredatorPrey_CellState.EMPTY);
-        }
-    }
+private boolean predatorEat(PredatorPrey_Cell cell) {
+	if (Objects.nonNull(cell)) {
+		cell.setNextState(PredatorPrey_CellState.EMPTY);
+		setNextState(new PredatorPrey_CellState(getCurrentState(), -1, getCurrentState().
+				getMaxStarvationTimer()));
+		return true;
+	}
+	return false;
+}
 
-    private void reproduce(PredatorPrey_Cell cell) {
-        if (Objects.nonNull(cell)) {
-            cell.setNextState(new PredatorPrey_CellState(getCurrentState(), getCurrentState().getMaxReproductionTimer(), -1));
-            setNextState(new PredatorPrey_CellState(getCurrentState(), getCurrentState().getMaxReproductionTimer(), getCurrentState().getMaxStarvationTimer()));
-        }
-    }
+private void move(PredatorPrey_Cell cell) {
+	if (Objects.nonNull(cell)) {
+		cell.setNextState(getCurrentState().getSuccessorState());
+		setNextState(PredatorPrey_CellState.EMPTY);
+	}
+}
 
-    private boolean nextStateEmpty() {
-        return getNextState().equals(PredatorPrey_CellState.EMPTY);
-    }
+private void reproduce(PredatorPrey_Cell cell) {
+	if (Objects.nonNull(cell)) {
+		cell.setNextState(new PredatorPrey_CellState(getCurrentState(), getCurrentState().getMaxReproductionTimer(), -1));
+		setNextState(new PredatorPrey_CellState(getCurrentState(), getCurrentState().getMaxReproductionTimer(), getCurrentState().getMaxStarvationTimer()));
+	}
+}
+
+private boolean nextStateEmpty() {
+	return getNextState().equals(PredatorPrey_CellState.EMPTY);
+}
 }
