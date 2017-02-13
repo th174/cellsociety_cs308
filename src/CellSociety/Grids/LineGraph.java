3d0811e0f68306cb5675d5d84fc57065ad8456fc
Salo Abraham Bandel <salo.abraham@duke.edu>
2017-02-12 23:04:41 -0500

    graph nearly done

--- src/CellSociety/Grids/LineGraph.java
+++ src/CellSociety/Grids/LineGraph.java
@@ -13,6 +13,7 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart.Series;
+import javafx.scene.input.TouchPoint.State;
 import javafx.stage.Stage;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,15 +25,15 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import CellSociety.Abstract_Cell;
+import CellSociety.Abstract_CellState;
 import CellSociety.Grids.SimulationGrid;
 import CellSociety.CellStateTimeline;
-import CellSocietyMain;
 
 
 public class LineGraph<E> {
 	private SimulationGrid mySimulation;
 	private Pane myRoot;
-	private HashMap<Integer, XYChart.Series<Number, Number>> mySeriesMap;
+	private HashMap<Integer, Series<Number, Number>> mySeriesMap;
 	private int xSeriesData = 0;
 
 	public <T> void Graph(SimulationGrid sim, Pane root) {
@@ -42,21 +43,21 @@ public class LineGraph<E> {
 		init();
 	}
 
-	private void init() {
+	private void init(){
 		//create axes and graph
 		//Need someway yo put in y axis the complete total of the grid
 		//get the name of the simulation
 		final NumberAxis xAxis = new NumberAxis();
-		final NumberAxis yAxis = new NumberAxis(0, mySimulation.countTotalOfState(stategit), mySimulation.countTotalOfState(state) / 100);
+		final NumberAxis yAxis = new NumberAxis(0, mySimulation., mySimulation.getDistinctCellStates());
 		xAxis.setLabel("Time");
 		yAxis.setLabel("Number of Cells");
 		final LineChart<Number, Number> graph = new LineChart<Number, Number>(xAxis, yAxis);
 		graph.setTitle("Cell State Populations");
 		//create objects to hold data
-		for (int stateInt : mySimulation.countTotalOfState(state)) {
+		for (Object s : mySimulation.getDistinctCellStates()) {
 			XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
-			series.setName(mySimulation.(stateInt));
-			mySeriesMap.put(stateInt, series);
+			//series.setName(mySimulation.getDistinctCellStates());
+			mySeriesMap.put((Integer) s, series);
 			graph.getData().add(series);
 		}
 		//Set where we want to put it
@@ -71,14 +72,14 @@ public class LineGraph<E> {
 	 *
 	 * @param grid - the simulation's grid
 	 */
-	public void updateGraph(GridPane grid) {
+	public void updateGraph(GridPane grid, SimulationGrid Sim) {
 		ArrayList<Integer> stateCount = new ArrayList<Integer>();
 		//number of states
 		for (int i = 0; i < mySeriesMap.size(); i++) {
 			stateCount.add(0);
+			Sim.countTotalOfState(mySeriesMap.get(i));
 		}
 		//How do I get total number of cell per each
-		
 		/*for (int i = 0; i < grid.getHeight(); i++) {
 			for (int j = 0; j < grid.getWidth(); j++) {
 				Cell cell = grid.getCell(i, j);
@@ -86,7 +87,7 @@ public class LineGraph<E> {
 				stateCount.set(cell.getCurrState(), oldCount + 1);
 			}
 		}
-		*/
+		 */
 		for (int state = 0; state < stateCount.size(); state++) {
 			mySeriesMap.get(state).getData().add(new XYChart.Data<Number, Number>(xSeriesData, stateCount.get(state)));
 		}
