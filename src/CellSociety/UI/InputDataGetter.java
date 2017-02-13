118ffa290e22c913ff0a682fdabddf05b7d6b0dc
Tim Overeem <to41@duke.edu>
2017-02-12 22:05:34 -0500

    Added comments to all public methods

--- src/CellSociety/UI/InputDataGetter.java
+++ src/CellSociety/UI/InputDataGetter.java
@@ -6,17 +6,38 @@ import CellSociety.Grids.SimulationGrid;
 
 public interface InputDataGetter {
 
+    /**
+     * @return simulationGrid of the animation
+     */
     SimulationGrid<? extends Abstract_Cell, ? extends Abstract_CellState> getSimulationGrid();
 
+    /**
+     * @return simulationType of the animation
+     */
     String getSimulationType();
 
+    /**
+     * @return framesPerSecond, i.e. speed of animation
+     */
     double getFramesPerSecond();
 
+    /**
+     * @return shape of cells in the animatino
+     */
     String getCellShape();
 
+    /**
+     * @return bound type of the grid
+     */
     String getGridBoundType();
 
+    /**
+     * @return neighbormode, i.e. which neighbors are being considered
+     */
     String getNeighborMode();
 
+    /**
+     * @return outline of the grid
+     */
     String getGridOutline();
 }
