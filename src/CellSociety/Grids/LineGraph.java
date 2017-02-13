43bf939d340fa319c86f1e43f7262ed8b69c32ce
Salo Abraham Bandel <salo.abraham@duke.edu>
2017-02-12 19:57:02 -0500

    mod to graph 2

--- src/CellSociety/Grids/LineGraph.java
+++ src/CellSociety/Grids/LineGraph.java
@@ -14,10 +14,23 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart.Series;
 import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.Pane;
+import CellSociety.Abstract_Cell;
+import CellSociety.Grids.SimulationGrid;
+import CellSociety.CellStateTimeline;
+import CellSocietyMain;
+
 
 public class LineGraph extends Application implements SimulationGridImpl {
-    private static final int MAX_DATA_POINTS = 6000;
-    
+	/* private static final int MAX_DATA_POINTS = 6000;
+
     private Series series;
     private int xSeriesData = 0;
     private ConcurrentLinkedQueue<Number> dataQ = new ConcurrentLinkedQueue<Number>();
@@ -30,7 +43,7 @@ public class LineGraph extends Application implements SimulationGridImpl {
         xAxis = new NumberAxis(0,MAX_DATA_POINTS,MAX_DATA_POINTS/10);
         xAxis.setForceZeroInRange(false);
         xAxis.setAutoRanging(false);
-        
+
         NumberAxis yAxis = new NumberAxis();
         yAxis.setAutoRanging(true);
 
@@ -99,5 +112,72 @@ public class LineGraph extends Application implements SimulationGridImpl {
         // update 
         xAxis.setLowerBound(xSeriesData-MAX_DATA_POINTS);
         xAxis.setUpperBound(xSeriesData-1);
-    }
-}
+  }*/  
+	private Simulation mySimulation;
+	private Pane myRoot;
+	private HashMap<Integer, XYChart.Series<Number, Number>> mySeriesMap;
+	private int xSeriesData = 0;
+
+	public Graph(Simulation sim, Pane root) {
+		mySimulation = sim;
+		myRoot = root;
+		mySeriesMap = new HashMap<Integer, XYChart.Series<Number, Number>>();
+		init();
+	}
+
+	private void init() {
+		//create axes and graph
+		final NumberAxis xAxis = new NumberAxis();
+		double maxY = mySimulation.getGridHeight() * mySimulation.getGridWidth();
+		final NumberAxis yAxis = new NumberAxis(0, maxY, maxY / 10);
+		xAxis.setLabel("Time");
+		yAxis.setLabel("Number of Cells");
+		final LineChart<Number, Number> graph = new LineChart<Number, Number>(xAxis, yAxis);
+		graph.setTitle("Cell State Populations");
+		//create objects to hold data
+		for (int stateInt : mySimulation.getStateMap().keySet()) {
+			XYChart.Series<Number, Number> series = new XYChart.Series<Number, Number>();
+			series.setName(mySimulation.getStateMap().get(stateInt));
+			mySeriesMap.put(stateInt, series);
+			graph.getData().add(series);
+		}
+		graph.setLayoutX(750);
+		graph.setLayoutY(130);
+		myRoot.getChildren().add(graph);
+	}
+
+	/**
+	 * loops through the Grid, calculates populations of each cell type, and updates value
+	 * called by animation.java for each step
+	 *
+	 * @param grid - the simulation's grid
+	 */
+	public void updateGraph(Grid grid) {
+		ArrayList<Integer> stateCount = new ArrayList<Integer>();
+		for (int i = 0; i < mySeriesMap.size(); i++) {
+			stateCount.add(0);
+		}
+		for (int i = 0; i < grid.getHeight(); i++) {
+			for (int j = 0; j < grid.getWidth(); j++) {
+				Cell cell = grid.getCell(i, j);
+				int oldCount = stateCount.get(cell.getCurrState());
+				stateCount.set(cell.getCurrState(), oldCount + 1);
+			}
+		}
+		for (int state = 0; state < stateCount.size(); state++) {
+			mySeriesMap.get(state).getData().add(new XYChart.Data<Number, Number>(xSeriesData, stateCount.get(state)));
+		}
+		xSeriesData += 1;
+	}
+
+	/**
+	 * removes the Graph object from the scene
+	 * this is called by Animation.java when the simulation type changes
+	 */
+	public void clearGraph() {
+		myRoot.getChildren().remove(myRoot.lookup("#" + myResources.getString("GraphID")));
+	}
+
+
+}	
+
